dbb6e2f1aa2e9bedc8d9cd7b4637224234c421b7
Vicktor Kemechedzhiev <kemechedzhiev@lider-pharm.ru>
2019-09-19 17:01:01 +0300

    Add new file

---
+++ profiler.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func profile() (int, string) {
+	var age int
+	var name string
+	fmt.Println("What's your name?")
+	fmt.Scan(&name)
+	fmt.Println("What's your age?")
+	fmt.Scan(&age)
+	return age, name
+}
