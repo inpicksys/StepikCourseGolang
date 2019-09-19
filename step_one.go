0d866db2c68d4580bc365ddf91be7cba10e4fe31
Vicktor Kemechedzhiev <kemechedzhiev@lider-pharm.ru>
2019-09-19 17:20:18 +0300

    Выполнено тестовое задание 1

--- step_one.go
+++ step_one.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 3; i++ {
-		fmt.Println("I like Go!")
-	}
-	age, name := profile()
-	fmt.Println(name, age)
+	//for i := 0; i < 3; i++ {
+	//	fmt.Println("I like Go!")
+	//}
+	//age, name := profile()
+	//fmt.Println(name, age)
+	a := stepOneExtendedWork()
+	fmt.Println(a)
 }
--- step_one_extended.go
+++ step_one_extended.go
@@ -1,3 +1,14 @@
 //TODO Напишите программу, которая последовательно делает следующие операции с каждым числом:
 //Число умножается на 2;
 //Затем к числу прибавляется 100.
+package main
+
+import "fmt"
+
+func stepOneExtendedWork() int {
+	var a int
+	fmt.Scan(&a)
+	a *= 2
+	a += 100
+	return a
+}
