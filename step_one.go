06d6c8aa75ab38b4fa6104e60287bd01acef375c
Vicktor Kemechedzhiev <kemechedzhiev@lider-pharm.ru>
2019-09-24 17:54:11 +0300

    Выполнено тестовое задание 6

--- step_one.go
+++ step_one.go
@@ -1,17 +1,20 @@
 package main
 
 func main() {
-	//for i := 0; i < 3; i++ {
-	//	fmt.Println("I like Go!")
-	//}
-	//age, name := profile()
-	//fmt.Println(name, age)
-	//a := stepOneExtendedWork()
-	//fmt.Println(a)
-	//fmt.Println(squarer())
-	//fmt.Println(last_digit())
-	//fmt.Println(pre_last_digit())
-	//var h, m int = clockReader()
-	//fmt.Print("It is ", h, " hours ", m, " minutes.")
-	some_func()
+	/*
+		for i := 0; i < 3; i++ {
+			fmt.Println("I like Go!")
+		}
+		age, name := profile()
+		fmt.Println(name, age)
+		a := stepOneExtendedWork()
+		fmt.Println(a)
+		fmt.Println(squarer())
+		fmt.Println(last_digit())
+		fmt.Println(pre_last_digit())
+		var h, m int = clockReader()
+		fmt.Print("It is ", h, " hours ", m, " minutes.")
+		some_func()
+	*/
+	if_example()
 }
