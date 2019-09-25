08050eeaac22eab66f38eb2b1fe82a454096348b
Vicktor Kemechedzhiev <kemechedzhiev@lider-pharm.ru>
2019-09-25 15:03:50 +0300

    Выполнено тестовое задание 10

--- step_one.go
+++ step_one.go
@@ -3,25 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	/*
-		for i := 0; i < 3; i++ {
-			fmt.Println("I like Go!")
-		}
-		age, name := profile()
-		fmt.Println(name, age)
-		a := stepOneExtendedWork()
-		fmt.Println(a)
-		fmt.Println(squarer())
-		fmt.Println(lastDigit())
-		fmt.Println(preLastDigit())
-		var h, m int = clockReader()
-		fmt.Print("It is ", h, " hours ", m, " minutes.")
-		some_func()
-		if_example()
-		sameDigits()
-		fmt.Println(firstDigit())
-	*/
-	if luckyTicket() {
+	if isLeapYear() {
 		fmt.Println("YES")
 	} else {
 		fmt.Println("NO")
