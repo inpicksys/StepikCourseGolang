1a7d8eb2e68a4e8275b63860d2b46ff7266df53d
Vicktor Kemechedzhiev <kemechedzhiev@lider-pharm.ru>
2019-09-26 16:12:11 +0300

    Выполнено тестовое задание 13

--- lesson1_test13.go
+++ lesson1_test13.go
@@ -1,1 +1,14 @@
+package main
+
+import "fmt"
+
 //TODO Требуется написать программу, при выполнении которой с клавиатуры считываются два натуральных числа A и B (каждое не более 100, A < B). Вывести сумму всех чисел от A до B  включительно.
+
+func summer() uint {
+	var a, b, result uint
+	fmt.Scan(&a, &b)
+	for i := a; i <= b; i++ {
+		result += i
+	}
+	return result
+}
