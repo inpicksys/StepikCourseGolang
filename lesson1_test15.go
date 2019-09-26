8444b9316e2a814424405d26313d70e5828470f9
Vicktor Kemechedzhiev <kemechedzhiev@lider-pharm.ru>
2019-09-26 16:35:13 +0300

    Выполнено тестовое задание 15

--- lesson1_test15.go
+++ lesson1_test15.go
@@ -1,1 +1,20 @@
+package main
+
+import "fmt"
+
 //TODO Последовательность состоит из натуральных чисел и завершается числом 0. Определите количество элементов этой последовательности. которые равны ее наибольшему элементу.
+
+func equalsMaxCounter() uint {
+	var a, max, counter uint
+	fmt.Scan(&a)
+	for a != 0 {
+		if a > max {
+			max = a
+			counter = 1
+		} else if max == a {
+			counter++
+		}
+		fmt.Scan(&a)
+	}
+	return counter
+}
--- step_one.go
+++ step_one.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(dividesToEight())
+	fmt.Println(equalsMaxCounter())
 }
