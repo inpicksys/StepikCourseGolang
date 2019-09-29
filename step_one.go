e97726a812271ba15abc2f29f62cced4d7ac2059
Виктор М. Кемечеджиев <kemechedzhiev@lider-pharm.ru>
2019-09-29 13:49:14 +0300

    Выполнено тестовое задание 19

--- step_one.go
+++ step_one.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var x, p, y uint
-	fmt.Scan(&x, &p, &y)
-	result := bankPercents(x, p, y)
-	fmt.Println(result)
+	var x, p uint
+	fmt.Scan(&x, &p)
+	areDigitsInOther(x, p)
 }
