3382a093f7ba1aabc3294e9b545aeb23010fe8d5
Vicktor Kemechedzhiev <kemechedzhiev@lider-pharm.ru>
2020-03-23 17:49:12 +0300

    New tasks completed

--- Lesson2/executor.go
+++ Lesson2/executor.go
@@ -1,12 +1,14 @@
 package Lesson2
 
-import "fmt"
-
 func Executor() {
 	//text := "Surprise, motherfucker!"
 	//f(text)
 	//a, b, c, d := 4, 5, 6, 7
 	//fmt.Println(minimumFromFour(a, b, c, d))
-	x, y, z := 0, 0, 1
-	fmt.Println(vote(x, y, z))
+	//x, y, z := 0, 0, 1
+	//fmt.Println(vote(x, y, z))
+	//x := 4
+	//fmt.Println(fibonacci(x))
+	x, y := 2, 4
+	test2(&x, &y)
 }
