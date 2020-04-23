e02a63cc707666d08d87cab9f9bef38990ad7aad
Vicktor Kemechedzhiev <kemechedzhiev@lider-pharm.ru>
2020-04-23 13:03:47 +0300

    Задание выполнено. Курс переходит к скользящим релизам.

---
+++ Lesson3/executor.go
@@ -0,0 +1,5 @@
+package Lesson3
+
+func Executor() {
+	cachedWork()
+}
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
-import "StepikCourseGolang/Lesson2"
+import "StepikCourseGolang/Lesson3"
 
 func main() {
 	//Lesson1.Executor()
-	Lesson2.Executor()
+	//Lesson2.Executor()
 	//Practice.Executor()
+	Lesson3.Executor()
 }
